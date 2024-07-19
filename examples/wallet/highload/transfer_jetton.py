48b61c8e3ac7b3b89a35fe3fba5750d5eddf50be
nessshon <shonness98@gmail.com>
2024-07-19 17:05:17 +0500

    Add `transfer_jetton` examples.

---
+++ examples/wallet/highload/transfer_jetton.py
@@ -0,0 +1,45 @@
+from pytoniq_tools.client import TonapiClient
+from pytoniq_tools.wallet import HighloadWalletV2
+from pytoniq_tools.wallet.data import TransferJettonData
+
+API_KEY = ""
+IS_TESTNET = True
+
+MNEMONIC = []
+
+
+async def main() -> None:
+    client = TonapiClient(api_key=API_KEY, is_testnet=IS_TESTNET)
+    wallet, public_key, private_key, mnemonic = HighloadWalletV2.from_mnemonic(MNEMONIC, client)
+
+    tx_hash = await wallet.transfer_jetton(
+        data_list=[
+            TransferJettonData(
+                destination="EQC-3ilVr-W0Uc3pLrGJElwSaFxvhXXfkiQA3EwdVBHNNess",
+                jetton_master_address="EQAiboDEv_qRrcEdrYdwbVLNOXBHwShFbtKGbQVJ2OKxY0to",
+                jetton_amount=0.01,
+                comment="Hello from pytoniq-tools!"
+            ),
+            TransferJettonData(
+                destination="EQC-3ilVr-W0Uc3pLrGJElwSaFxvhXXfkiQA3EwdVBHNNess",
+                jetton_master_address="EQAiboDEv_qRrcEdrYdwbVLNOXBHwShFbtKGbQVJ2OKxY0to",
+                jetton_amount=0.01,
+                comment="Hello from pytoniq-tools!"
+            ),
+            TransferJettonData(
+                destination="EQC-3ilVr-W0Uc3pLrGJElwSaFxvhXXfkiQA3EwdVBHNNess",
+                jetton_master_address="EQAiboDEv_qRrcEdrYdwbVLNOXBHwShFbtKGbQVJ2OKxY0to",
+                jetton_amount=0.01,
+                comment="Hello from pytoniq-tools!"
+            )
+        ]
+    )
+
+    print("Successfully transferred!")
+    print(f"Transaction hash: {tx_hash}")
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
---
+++ examples/wallet/v3/transfer_jetton.py
@@ -0,0 +1,31 @@
+from pytoniq_tools.client import TonapiClient
+from pytoniq_tools.wallet import WalletV3R2
+
+API_KEY = ""
+IS_TESTNET = True
+
+MNEMONIC = []
+
+DESTINATION_ADDRESS = "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c"
+JETTON_MASTER_ADDRESS = "EQAiboDEv_qRrcEdrYdwbVLNOXBHwShFbtKGbQVJ2OKxY0to"  # noqa
+
+
+async def main() -> None:
+    client = TonapiClient(api_key=API_KEY, is_testnet=IS_TESTNET)
+    wallet, public_key, private_key, mnemonic = WalletV3R2.from_mnemonic(MNEMONIC, client)
+
+    tx_hash = await wallet.transfer_jetton(
+        destination=DESTINATION_ADDRESS,
+        jetton_master_address=JETTON_MASTER_ADDRESS,
+        jetton_amount=0.01,
+        comment="Hello from pytoniq-tools!"
+    )
+
+    print("Successfully transferred!")
+    print(f"Transaction hash: {tx_hash}")
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
